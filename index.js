e0a43971b20755621c7bf04c412cbfdeb26a27d6
UrvikP <urvik95@gmail.com>
2026-05-15 19:39:22 -0400

    challenge # 3

--- index.js
+++ index.js
@@ -29,21 +29,30 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++){
         // create a new div element, which will become the game card
-
+        const game = document.createElement(<div></div>)
 
         // add the class game-card to the list
+        game.classList.add("game-card")
 
 
         // set the inner HTML using a template literal to display some info 
+        game.innerHTML = `
+        ${<h1>{game.getElementById("name")}</h1>}
+        ${<div/>}  
+        ${<img id='game-iamge' src={game.getElementById("img")} />}
+        ${<div/>} 
+        ${<p>{game.getElementById("description")}</p> }
+        `
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
-
         // append the game to the games-container
+        gamesContainer.append(game)
+    }
+    return gamesContainer
 
 }
 
